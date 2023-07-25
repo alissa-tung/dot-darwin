70d0fa2f66cc9ee46a2a20a13e4de43719862c42
Alissa Tung <alissa-tung@outlook.com>
2023-07-26 00:12:24 +0800

    *: more pkgs

--- src/darwin-configuration.nix
+++ src/darwin-configuration.nix
@@ -6,7 +6,7 @@
   pkgs = import <nixpkgs> {config.allowUnfree = true;};
 in ({
     environment.systemPackages = with pkgs;
-      [neovim git gcc gnumake alejandra fd deno clang-tools yamlfmt rustup elan gmp coreutils shellcheck jq unar]
+      [neovim git gcc gnumake alejandra fd deno clang-tools yamlfmt rustup elan gmp coreutils shellcheck jq unar bottom rlwrap]
       ++ [(import ../pkgs/vscode.nix {inherit pkgs;})]
       ++ [
         (
